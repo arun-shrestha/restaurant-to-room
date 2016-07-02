44e860ea1fcb88337153669c49a1e3c3713ef0b2
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-02 17:45:21 +1000

    add client side validation

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../services/user-service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// GET /users/view
 router.get('/create', function(req, res, next) {
   var vm = {
     title: 'Create an account'
   };
   res.render('users/create', vm);
+});
+
+router.post('/create', function(req, res, next) {
+  userService.addUser(req.body, function(err) {
+    if (err) {
+      var vm = {
+        title: 'Create an account',
+        input: req.body,
+        error: err
+      };
+      delete vm.input.password;
+      return res.render('users/create', vm);
+    }
+    res.redirect('/orders');
+  });
 });
 
 module.exports = router;
